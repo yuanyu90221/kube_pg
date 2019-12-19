e984d68a4c362560f889429db168fa4c6438d433
json <yuanyu90221@gmail.com>
2019-12-19 18:46:19 +0800

    update

---
+++ delete_postgresql.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+kubectl delete service postgres 
+kubectl delete deployment postgres
+kubectl delete configmap postgres-config
+kubectl delete persistentvolumeclaim postgres-pv-claim
+kubectl delete persistentvolume postgres-pv-volume
