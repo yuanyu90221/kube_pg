5d6b736d6be79331dc190d34f0c58307f66ada1a
json <yuanyu90221@gmail.com>
2019-12-20 11:07:57 +0800

    update configmap

--- create_postgresql.sh
+++ create_postgresql.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 source ./.env
-export HOST_DIR=${HOST_DIR}
-export POSTGRES_DB=${POSTGRES_DB}
-export POSTGRES_USER=${POSTGRES_USER}
-export POSTGRES_PASSWORD=${POSTGRES_PASSWORD}
-echo ${HOST_DIR}
-echo ${POSTGRES_DB}
-echo ${POSTGRES_USER}
-echo ${POSTGRES_PASSWORD}
+#echo $HOST_DIR
+export HOST_DIR=$HOST_DIR
+export POSTGRES_DB=$POSTGRES_DB
+export POSTGRES_USER=$POSTGRES_USER
+export POSTGRES_PASSWORD=$POSTGRES_PASSWORD
+echo $HOST_DIR
+echo $POSTGRES_DB
+echo $POSTGRES_USER
+echo $POSTGRES_PASSWORD
 kubectl create -f postgres-configmap.yaml
 kubectl create -f postgres-storage.yaml
 kubectl create -f postgres-deployment.yaml
